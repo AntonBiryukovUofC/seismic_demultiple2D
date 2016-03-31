cfd8617f582cdddd3fa320b7cef828f521e9c39e
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2016-03-31 09:21:13 -0600

    2nd

--- Matlab_Data/sofi2D_FW/sofi2D_forward_modelling.m
+++ Matlab_Data/sofi2D_FW/sofi2D_forward_modelling.m
@@ -11,12 +11,13 @@ Vp = [3000 5000 4000 7000];
 Vs = Vp/1.7;
 rho =[2000 2000 2000 2000];
 depth = [30 20 30 30] % in # of cells !
-NXX = 100; % Length in X in # of cells !
+abframe = 15;
+NXX = 100+2*abframe; % Length in X in # of cells !
+NYY = sum(depth) + abframe;
 name = 'sofi2D_test';
 % Time params
 T_max=2.5;
 dt_mod = 0.2E-3;
-abframe = 15;
 % Source parameters
 source_frequency = 3 ; % [Hz]
 vs_min = min(Vs); % Model size (source- dependent)
@@ -25,9 +26,9 @@ discr = 15;
 
 dh = round(min_wavelength/discr);
 
-LENGTH_IN_X = NXX*dh + 2*abframe*dh;
+LENGTH_IN_X = NXX*dh;
 
-LENGTH_IN_Y = sum(depth) + abframe;
+LENGTH_IN_Y = NYY;
 first_rec = (abframe+1) * dh;
 last_rec = (abframe+1 + NXX) * dh;
  % Shape and type of the input signal :
@@ -43,97 +44,48 @@ dlmwrite('source.dat', source_matrix, 'delimiter', '\t');
 receiver_matrix = [x_rec; y_rec]';
 dlmwrite('receiver.dat', source_matrix, 'delimiter', '\t');
 
-
-return
-
-
 % Snapshot & receivers params 
-num_rec= 100;
 IDXX=1;
 IDYY=1;
 snap=0;
 SETYPE = 1;
-
-fileID=fopen('receiver.dat','w');
-
-x1 = (abframe+4)*dh;
-x2 = LENGTH_IN_X - x1;
-%x1=100;
-%x2=100;
-y1 = 2*dh;
-x=linspace(x1,x2,num_rec);
-
-y=ones(1,length(x))*y1;
-%x=[x 20*dh];
-%y=[y 9*dh];
-P_rec=[x', y'];
-dlmwrite('receiver.dat',P_rec,'delimiter','\t','precision',7);
-fclose(fileID);
-
-
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-NX = round(LENGTH_IN_X/dh)+1;
-NY = round(LENGTH_IN_Y/dh)+1;
-if mod(NY,2) == 1
-NY = NY -1;
+vps = zeros(NYY,NXX);
+vss = zeros(NYY,NXX);
+vps(1:depth(1),:) = Vp(1);
+vss(1:depth(1),:) = Vs(1);
+levels= cumsum(depth);
+
+for i=numel(Vp):-1:1
+	vps(1:levels(i),:) = Vp(i);
+	vss(1:levels(i),:) = Vs(i);
+	
+	
 end
-if mod(NX,2) == 1
-NX = NX -1;
-end
-vp = zeros(NY,NX);
-[X,Y] = meshgrid(0:dh:(NX-1)*dh,0:dh:(NY-1)*dh);
-vp = 0.*X;
-trial_depth = linspace(0,hypo_DE_depth(end),NY);
-
-ind = find(trial_depth<hypo_DE_depth(1));
-vp(ind,:) = hypo_DE_Vp(1)*ones(numel(ind),size(X,2));
-
-for i=2:numel(hypo_DE_Vp)
-ind = find(trial_depth > hypo_DE_depth(i-1) & trial_depth <=hypo_DE_depth(i) );
-vp(ind,:) = hypo_DE_Vp(i)*ones(numel(ind),size(X,2));
-end
-
-if UNIFY==1
-vel = linspace(min(min(vp)),max(max(vp)),NY);
-for i=1:NY
-vp(i,:)=vel(i);
-end
-
-end
+vps(levels(end):end,:) = Vp(end);
+vss(levels(end):end,:) = Vs(end);
 
 
-vs=vp/1.83;
 
-rho = 2000 + 0.*vp; % Density model
 
-vp=kf.*vp;
-vs=kf.*vs;
 
-qp = 0.*vp + 250;
-qs = 0.*vp + 250;
 
-figure(1)
-imagesc(X,Y,vp);
+
+hf = figure(1);
+set(hf,'Position',[10 10 600 800])
+imagesc((1:NXX)*dh,(1:NYY)*dh,vps);
 cmap = colormap('gray');
 cmap=flipud(cmap);
 colormap(cmap)
 hold all
-plot(P_rec(:,1),P_rec(:,2),'vr','MarkerSize',10,'MarkerFaceColor','green');
-plot(x_src,y_src,'*g','MarkerSize',10,'MarkerFaceColor','green');
+for i=1:numel(Vp)
+	hl = line([0 NXX*dh],[levels(i)*dh levels(i)*dh],'Color','r','LineWidth',4.5);
+end
 
-RHO_MAX=2000*1.1
-VP_MAX=max(hypo_DE_Vp)*kf*1.1;
-VS_MAX=VP_MAX./1.7;
-VP_MIN =min(hypo_DE_Vp)*0.9;
-VS_MIN =VP_MIN/1.7;
+plot(x_rec,y_rec,'vr','MarkerSize',10,'MarkerFaceColor','red');
 
+return
 
-RTM_MOD_ON =0;
-RTM_ON =0;
-TIMELAPSE_ON = 0;
-FILT_SIZE_CYL=7;
-RADIUS_TAPER = FILT_SIZE_CYL*dh;
 % Writing into the template file 
 fileID=fopen('DENISE_forward_template.inp','r');
 FILE=fread(fileID, [1 inf] ,'*char');
